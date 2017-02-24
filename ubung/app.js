2c4be7f643555e008b68c89008f641338e0e842e
Parvathy Menon <parvathy.menon1997@gmail.com>
2017-02-23 18:23:31 -0800

    angular should now be clickable

--- ubung/app.js
+++ ubung/app.js
@@ -6,24 +6,24 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
-// Connect to Database
+//Connect to Database
 mongoose.connect(config.database);
 
-// On Connection
+//On Connection
 mongoose.connection.on('connected', () => {
-	console.log('Connected to database ' + config.database);
+  console.log('Connected to database '+config.database);
 });
 
-// On error
-mongoose.connection.on('connected', (err) => {
-	console.log('Database Error ' + err);
+//On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
 });
 
 const app = express();
 
 const users = require('./routes/users');
 
-//Port Number
+//Port number
 const port = 3000;
 
 //CORS Middleware
@@ -44,16 +44,15 @@ require('./config/passport')(passport);
 app.use('/users', users);
 
 //Index Route
-app.get('/', (request,response) => {
-	response.send('Invalid Endpoint');
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
 });
 
-//Default route
-app.get('*', (request,response) => {
-	response.sendFile(path.join(__dirname, 'public/index.html'))
+app.get('*', () => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
 //Start Server
 app.listen(port, () => {
-	console.log('Server started on port ' + port);
+  console.log('Server started on port '+port);
 });
--- ubung/routes/users.js
+++ ubung/routes/users.js
@@ -2,26 +2,25 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const config = require ('../config/database');
+const config = require('../config/database');
 const User = require('../models/user');
 
-
-//Register
+// Register
 router.post('/register', (req, res, next) => {
-	let newUser = new User({
-		name: req.body.name,
-		email: req.body.email,
-		username: req.body.username,
-		password: req.body.password
-	});
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
 
-	User.addUser(newUser, (err, user) => {
-		if(err) {
-			res.json({success: false, msg:'Failed to register user'});
-		} else {
-			res.json({success: true, msg:'User registered'});
-		}
-	});
+  User.addUser(newUser, (err, user) => {
+    if(err) {
+      res.json({success: false, msg:'Failed to register user'});
+    } else {
+      res.json({success: true, msg:'User registered'});
+    }
+  });
 });
 
 //Authenticate
@@ -59,9 +58,9 @@ router.post('/authenticate', (req, res, next) => {
 	});
 });
 
-//Profile
-router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
-	res.json({user: req.user});
+// Profile
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+  res.json({user: req.user});
 });
 
 module.exports = router;
