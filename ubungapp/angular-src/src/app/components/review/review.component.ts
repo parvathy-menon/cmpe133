c56ea5a0818e6a0405f87d5514d80f62a606adc5
vjdhanota <vjd209@gmail.com>
2017-04-22 13:10:22 -0700

    ability to add reviews to a workout has been added

---
+++ ubungapp/angular-src/src/app/components/review/review.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {ReviewsService} from '../../services/reviews.service'
+
+@Component({
+  selector: 'app-review',
+  templateUrl: './review.component.html',
+  styleUrls: ['./review.component.css']
+})
+export class ReviewComponent implements OnInit {
+  reviews:Array<Object>;
+  workoutId:String;
+
+  constructor(private router:Router, 
+              private reviewsService:ReviewsService,
+              private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+
+    this.activatedRoute.params.subscribe((params: Params) => {
+        this.workoutId = params['_id'];
+      });
+
+    this.reviewsService.getReviews(this.workoutId).subscribe(reviews => {
+      var array = Object.keys(reviews).map((key) => {return reviews[key]});
+      this.reviews = array[1];
+      console.log(this.reviews[0]);
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
+  }
+
+}
