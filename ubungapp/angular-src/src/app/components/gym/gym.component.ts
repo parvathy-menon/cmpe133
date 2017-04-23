16f0d948250d36d98c7d8a4b40af925b4497377f
emilylxie <emiylxie@gmail.com>
2017-04-22 21:21:08 -0700

    search gym by location done

--- ubungapp/angular-src/src/app/components/gym/gym.component.ts
+++ ubungapp/angular-src/src/app/components/gym/gym.component.ts
@@ -8,14 +8,31 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './gym.component.html',
   styleUrls: ['./gym.component.css']
 })
-export class GymComponent implements OnInit {
 
-  constructor(private router: Router, private gymService: GymService, private authService: AuthService) { }
+export class GymComponent implements OnInit {
+  location: String;
+  constructor(
+    private router: Router,
+    private gymService: GymService,
+    private authService: AuthService
+  ) { }
 
   gyms: Array<Object>;
 
   ngOnInit() {
-    this.gymService.getGyms().subscribe(gyms => {
+    this.gymService.getGyms('http://localhost:3000/gyms/95113').subscribe(gyms => {
+      var array = Object.keys(gyms).map((key) => {return gyms[key]});
+      this.gyms = array[0];
+      this.gyms = gyms.list;
+    }, err => {
+      console.log(err);
+      return false;
+    });
+  }
+
+  onSearchSubmit() {
+    var url = 'http://localhost:3000/gyms/'+this.location;
+    this.gymService.getGyms(url).subscribe(gyms => {
       var array = Object.keys(gyms).map((key) => {return gyms[key]});
       this.gyms = array[0];
       this.gyms = gyms.list;
--- ubungapp/angular-src/src/app/services/gym.service.ts
+++ ubungapp/angular-src/src/app/services/gym.service.ts
@@ -4,15 +4,14 @@ import { Http, Headers } from '@angular/http';
 @Injectable()
 export class GymService {
 
-  constructor(private http: Http) { 
+  constructor(private http: Http) {
 
   }
 
-  getGyms() {
+  getGyms(url) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-
-    return this.http.get('http://localhost:3000/gyms/95113', { headers: headers })
+    return this.http.get(url, { headers: headers })
       .map(res => res.json());
   }
 
