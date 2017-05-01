f58cdf4469c8a2e7a4e36c0c1b682af9a530b06e
emilylxie <emilylxie@gmail.com>
2017-05-01 13:31:52 -0700

    delete Review done

--- ubungapp/angular-src/src/app/components/review/review.component.ts
+++ ubungapp/angular-src/src/app/components/review/review.component.ts
@@ -10,8 +10,9 @@ import {ReviewsService} from '../../services/reviews.service'
 export class ReviewComponent implements OnInit {
   reviews:Array<Object>;
   workoutId:String;
+  _id: String;
 
-  constructor(private router:Router, 
+  constructor(private router:Router,
               private reviewsService:ReviewsService,
               private activatedRoute: ActivatedRoute) { }
 
@@ -25,6 +26,28 @@ export class ReviewComponent implements OnInit {
       var array = Object.keys(reviews).map((key) => {return reviews[key]});
       this.reviews = array[1];
       console.log(this.reviews[0]);
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
+  }
+
+  onDeleteSubmit(review){
+    var id = review._id;
+    this.reviewsService.deleteReview(id).subscribe(reviews => {
+      this.activatedRoute.params.subscribe((params: Params) => {
+        this.workoutId = params['_id'];
+      });
+      this.reviewsService.getReviews(this.workoutId).subscribe(reviews => {
+        var array = Object.keys(reviews).map((key) => {return reviews[key]});
+        this.reviews = array[1];
+        console.log(this.reviews[0]);
+      },
+      err => {
+        console.log(err);
+        return false;
+      });
     },
     err => {
       console.log(err);
